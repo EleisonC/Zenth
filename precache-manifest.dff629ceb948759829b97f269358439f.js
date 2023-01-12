6a706f854d5bf9ed119842eb9b37ddec79778d50
EleisonC <ckalule7@gmail.com>
2023-01-12 23:13:24 +0200

    Updates

---
+++ precache-manifest.dff629ceb948759829b97f269358439f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2db9e7aa847ede2203e9",
+    "url": "/Zenth/static/css/main.3ce6d201.chunk.css"
+  },
+  {
+    "revision": "2db9e7aa847ede2203e9",
+    "url": "/Zenth/static/js/main.2db9e7aa.chunk.js"
+  },
+  {
+    "revision": "89c8133573e3f9a989ac",
+    "url": "/Zenth/static/css/1.4d770ace.chunk.css"
+  },
+  {
+    "revision": "89c8133573e3f9a989ac",
+    "url": "/Zenth/static/js/1.89c81335.chunk.js"
+  },
+  {
+    "revision": "06e13eec3489b41587a1",
+    "url": "/Zenth/static/js/runtime~main.06e13eec.js"
+  },
+  {
+    "revision": "70b8522489b2c8d1090e60caa069f649",
+    "url": "/Zenth/static/media/close-up-photo-of-adidas-shoes-1661470.70b85224.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Zenth/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Zenth/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Zenth/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Zenth/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "2caaabbb91b86b1314398f134c1715ed",
+    "url": "/Zenth/static/media/person-holding-nike-sb-suede-low-top-sneaker-1503009.2caaabbb.jpg"
+  },
+  {
+    "revision": "daa4566a317be37061c3196a1f0de3b6",
+    "url": "/Zenth/index.html"
+  }
+];
